ce6b0d052e583ffe708f0a0dc83ca6dd9f41ef91
Douglas Vinicius Vicente <douglas.vinicius001@gmail.com>
2022-03-21 12:12:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+namespace Teste_4
+{
+  class program
+  {
+    
+    public static void Main(string []args)
+    {
+      int ValorDesconto,ValorTotal;
+
+      ValorDesconto = 0;
+    WriteLine("Digite o valor da compra: ");
+    var Valor = int.Parse(ReadLine());
+    
+        if (Valor >=300 && Valor <=2200)
+        {
+        ValorDesconto = Valor -200;
+        ValorDesconto = ValorDesconto/100;
+        ValorTotal = Valor - ValorDesconto;
+        WriteLine($"O valor da compra foi de R${Valor}; A porcentagem de descondo adquirida foi de {ValorDesconto}%; O total da sua compra já com desconto é R${ValorTotal}.");
+        }
+        if (Valor >2200)
+        {
+        ValorDesconto = (Valor/100)*20;
+        ValorTotal = Valor - ValorDesconto;
+        WriteLine($"O valor da compra foi de R${Valor}; A porcentagem de descondo adquirida foi de 20%; O total da sua compra já com desconto é R${ValorTotal}.");
+        }
+        else if (Valor <300)
+        {
+          WriteLine ("Infelizmente você não adquiriu descontos :( ");
+        }
+    }
+  }
+}
